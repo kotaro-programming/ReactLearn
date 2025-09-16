0ddfa969cf242e13b43af266d2c271d933518326
impudentlife <impudentlife.1984@gmail.com>
2025-09-16 19:26:51 +0900

    20250916２回目のコミット

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,14 +1,14 @@
 // src/App.tsx
 import { useState } from "react";
+import Greeting from "./components/Greeting";
 
 export default function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <main style={{ fontFamily: "system-ui", padding: 24 }}>
-      <h1>はじめての React（TypeScript）</h1>
-      <p>カウント: {count}</p>
-      <button onClick={() => setCount((v) => v + 1)}>+1</button>
+    <main style={{ fontFamily: "system-ui", padding: 24}}>
+      <h1>部品化の第一歩</h1>
+      <Greeting name="太郎" />
     </main>
   );
 }
